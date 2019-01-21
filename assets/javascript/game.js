7926812594f380c6e0a3ce32314afa50236b9c18
Danelle Pontzious <danellepontzious@Unknown.domain>
2019-01-20 21:28:53 -0700

    updated if statement

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+// var counter = 0;
+// var wins = 0;
+// var losses = 0;
+// var userCount = 0;
+
+// var targetNum = Math.floor(Math.random() * 100);
+// console.log(targetNum);
+
+// $("img1").on("click") = function(event){
+//     event = Math.floor(Math.random() * 12 + 1);
+// }
+
+// var box1 = event.key;
+// console.log(box1);
+
+// // ("#img1").on("click", function () {
+// //     userCount++;
+// //     console.log(userCount);
+// // });
+// // $("img2").on("click", function(){
+// //     userCount++;
+// // // },
+// //     document.getElementById("wins").innerHTML = wins;
+// //     document.getElementById("losses").innerHTML = losses;
+// //     document.getElementById('counter').innerHTML = counter;
+// //     document.getElementById('guess').innerHTML = user;
+// document.getElementById('img1').innerHTML = box1;
+
+// var numOption = Math.floor(Math.random() * 12) + 1;
+// console.log(numOption);
+
+// var counter = 
+
+// for (var i = 0; i < numOption.length; i++){
+
+
+// var 
+
+// if (counter === targetNum){
+//     wins++
+// }
+// else (counter >= targetNum){
+//     losses++;
+// }
