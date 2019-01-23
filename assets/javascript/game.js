3804cfb977008124e98170996c24a606c035517f
Danelle Pontzious <danellepontzious@Unknown.domain>
2019-01-22 21:07:43 -0700

    box var change when game restarts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,32 +7,42 @@ $(document).ready(function () {
     var box2 = Math.floor(Math.random() * 10) + 1;
     var box3 = Math.floor(Math.random() * 10) + 1;
     var box4 = Math.floor(Math.random() * 10) + 1;
-    $(".box").on("click", function (event) {
-        var userGuess;
-        if (event.target.id === 'box1') {
-            userGuess = box1;
-        } else if (event.target.id === 'box2') {
-            userGuess = box2;
-        } else if (event.target.id === 'box3') {
-            userGuess = box3;
-        } else {
-            userGuess = box4;
-        }
-        counter += userGuess;
-        $(".userGuess").text(counter);
-        $(".random").text(targetNum);
-        if (counter === targetNum) {
-            wins++;
-            counter = 0;
-            targetNum = Math.floor(Math.random(19, 120) * (120 - 19) + 19);
-            console.log("wins", wins);
-        } else if (counter > targetNum) {
-            losses++;
-            counter = 0;
-            targetNum = Math.floor(Math.random(19, 120) * (120 - 19) + 19);
-            console.log("loss", losses);
-        }
-        $(".losses").text(losses);
-        $(".wins").text(wins);
-    });
+$(".box").on("click", function (event) {
+    var userGuess;
+    if (event.target.id === 'box1') {
+        userGuess = box1;
+    } else if (event.target.id === 'box2') {
+        userGuess = box2;
+    } else if (event.target.id === 'box3') {
+        userGuess = box3;
+    } else {
+        userGuess = box4;
+    }
+    counter += userGuess;
+
+    $(".userGuess").text(counter);
+    $(".random").text(targetNum);
+    if (counter === targetNum) {
+        wins++;
+        counter = 0;
+        targetNum = Math.floor(Math.random(19, 120) * (120 - 19) + 19);
+        box1 = Math.floor(Math.random() * 10) + 1;
+        box2 = Math.floor(Math.random() * 10) + 1;
+        box3 = Math.floor(Math.random() * 10) + 1;
+        box4 = Math.floor(Math.random() * 10) + 1;
+    } else if (counter > targetNum) {
+        losses++;
+        counter = 0;
+        targetNum = Math.floor(Math.random(19, 120) * (120 - 19) + 19);
+        console.log("loss", losses);
+        box1 = Math.floor(Math.random() * 10) + 1;
+        box2 = Math.floor(Math.random() * 10) + 1;
+        box3 = Math.floor(Math.random() * 10) + 1;
+        box4 = Math.floor(Math.random() * 10) + 1;
+
+    }
+    $(".losses").text(losses);
+    $(".wins").text(wins);
+    console.log("box1", box1);
+});
 });
